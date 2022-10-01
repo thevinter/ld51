cd553c4c475b1baf8e19699a30f42d9c2481f535
Brancatisano Evseev <nbrancatisanoevse@deloitte.com>
2022-10-01 14:40:46 +0200

    feat: add player scene

---
+++ Scripts/Player/MoveController.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 100
+const ACCELERATION = 500
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	if(input != Vector2.ZERO):
+		velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
+
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = move_and_slide(velocity)
+
